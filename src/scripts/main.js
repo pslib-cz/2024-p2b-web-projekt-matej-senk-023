5c9f37852d6c76a2c6e8498dda0df9de971e05e6
matej-senk-023 <matej.senk.023@pslib.cz>
2025-07-31 00:17:42 +0200

    swiper issue fixed

---
+++ src/scripts/main.js
@@ -0,0 +1,31 @@
+// core version + navigation, pagination modules:
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const swiper = new Swiper('.swiper', {
+  /*loop: true, /*musi byt false, protoze jinak se 2. obrazek nerenderuje ihned po nacteni stranky. coz je velka designova chyba. Takze objetuji looping a nechavam si centeredSlides*/
+  centeredSlides: true,
+  spaceBetween: 30,
+  slidesPerView: 1,
+  watchSlidesProgress: true,
+
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+    },
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
