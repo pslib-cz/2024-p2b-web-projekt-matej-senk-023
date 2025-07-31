8c6983be1e0746c273315ea6dbe1f98bd081002c
matej-senk-023 <matej.senk.023@pslib.cz>
2025-07-31 23:11:09 +0200

    about and contact page added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -29,3 +29,10 @@ const swiper = new Swiper('.swiper', {
   },
 });
 
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
